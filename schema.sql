42e19ba6d33d818660e13bc148b487fbb7f54cb9
Michaelfr22 <102773052+Michaelfr22@users.noreply.github.com>
2022-06-07 16:57:47 -0400

    Update: includes queries to create specified info

--- schema.sql
+++ schema.sql
@@ -88,3 +88,139 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 ---- view retirement info table
 select * from retirement_info;
+
+-- drop retirement_info table so it can be edited
+DROP TABLE retirement_info; 
+
+-- Create new table for retiring employees, including emp_no
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Check the table
+SELECT * FROM retirement_info;
+
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables; using aliases
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    de.to_date
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no;
+
+-- Use Left Join for retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = '9999-01-01';
+
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+
+-- List 1: Employee Information
+SELECT 
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	s.to_date
+INTO emp_info
+FROM employees AS e
+	INNER JOIN salaries AS s
+		ON (e.emp_no = s.emp_no)
+	INNER JOIN dept_emp AS de
+	ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+
+drop table manager_info;
+-- List 2: Managers by department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+-- List 3: Department Retirees
+SELECT ce.emp_no,
+	 ce.first_name,
+	 ce.last_name,
+	 d.dept_name
+INTO dept_info
+FROM current_emp as ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no);
+		
+
+-- Create Tailored List
+drop table sales_info;
+
+---- Sales_info
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	de.dept_no,
+	d.dept_name
+INTO sales_info
+FROM dept_emp AS de
+	INNER JOIN employees as e
+		ON (de.emp_no = e.emp_no)
+	RIGHT JOIN departments as d
+		ON (de.dept_no = d.dept_no)
+WHERE dept_name = 'Sales'; 
+-- view sales info
+select * from sales_info;
+
+---- Sales_Dev_info
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	de.dept_no,
+	d.dept_name
+INTO sales_dev_info
+FROM dept_emp AS de
+	INNER JOIN employees as e
+		ON (de.emp_no = e.emp_no)
+	RIGHT JOIN departments as d
+		ON (de.dept_no = d.dept_no)
+WHERE (dept_name = 'Sales' OR dept_name = 'Development'); 
+
+-- view sales_dev_info
+select * from sales_dev_info;
